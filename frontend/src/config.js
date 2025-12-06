f3beaa338c60a7ec5f7789487f5e9006946eff24
vedd19 <ved123official@gmail.com>
2025-12-06 20:35:49 +0530

    added the deployed backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const config = { BACKEND_URL: 'http://localhost:5000' };
+// export const config = { BACKEND_URL: 'http://localhost:5000' };
+export const config = { BACKEND_URL: 'https://eventmanagement-g3as.onrender.com' };
